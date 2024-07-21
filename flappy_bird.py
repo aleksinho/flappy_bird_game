2c9abddfef3a2cde6d612993a9f053a3f810dddc
aleksinho <aleks.gsyan@gmail.com>
2024-07-21 14:21:10 +0400

    setting background color

--- flappy_bird.py
+++ flappy_bird.py
@@ -11,14 +11,19 @@ class FlappyBird:
         pygame.init()
 
         self.clock = pygame.time.Clock()
+
         self.screen = pygame.display.set_mode((720, 480))
         pygame.display.set_caption("Flappy Bird")
+
+        self.bg_color = (84, 168, 50)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.bg_color)
 
             pygame.display.flip()
             self.clock.tick(60)
