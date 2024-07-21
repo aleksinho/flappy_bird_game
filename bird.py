198959b0962dc9c15710a12cfd3d9af06f7d4901
aleksinho <aleks.gsyan@gmail.com>
2024-07-21 16:50:49 +0400

    making bird class cleaner

--- bird.py
+++ bird.py
@@ -1,24 +1,28 @@
 import pygame
+from settings import Settings
 
 
 
 class Bird:
     def __init__(self, fb_game):
         """ Initialize the ship and set its starting position """
+        self.settings = Settings()
+
         self.screen = fb_game.screen
         self.screen_rect = fb_game.screen.get_rect()
 
         # load the bird image and get its rect
-        self.original_image = pygame.image.load('images/flappy_bird.png')
+        self.original_image = pygame.image.load(self.settings.bird_image)
+
         # changing the size of the bird image
-        bird_size = (200/4, 111/4)
-        self.resized_image = pygame.transform.scale(self.original_image, bird_size)
+        self.resized_image = pygame.transform.scale(self.original_image, self.settings.bird_size)
+
         # get the bird rect
         self.rect = self.resized_image.get_rect()
 
         # giving its starting position
-        self.rect.x = 60
-        self.rect.y = 240
+        self.rect.x = self.settings.x_start
+        self.rect.y = self.settings.y_start
 
     def blitme(self):
         """ Drawing the ship """
--- settings.py
+++ settings.py
@@ -2,8 +2,22 @@ import pygame
 
 
 
+
 class Settings:
     def __init__(self):
+        # screen size
         self.screen_width = 720
         self.screen_height = 480
+
+        # background color
         self.bg_color = (52, 192, 235)
+
+        # bird image
+        self.bird_image = 'images/flappy_bird.png' 
+
+        # bird size
+        self.bird_size = (200/4, 111/4)
+
+        # bird starting position
+        self.x_start = 60
+        self.y_start = 240
